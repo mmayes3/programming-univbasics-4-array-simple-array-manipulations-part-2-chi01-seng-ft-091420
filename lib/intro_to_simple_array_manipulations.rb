3c0217644e884c11e3d4a5e4cc8e95620337eb01
classy-tier-0049 <mauricelynell@gmail.com>
2020-08-29 21:13:36 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,29 @@
+def using_concat(array1, array2)
+  array1.concat(array2)
+  return array1
+end
+
+def using_insert(array, element)
+  array.insert(4, element)
+  return array
+end
+
+def using_uniq(array)
+  new_array = array.uniq
+  return new_array
+end
+
+def using_flatten(array)
+  new_array = array.flatten
+  return new_array
+end
+
+def using_delete(array, string)
+  array.delete(string)
+  return array
+end
+
+def using_delete_at(array, integer)
+  new_array = array.delete_at(integer)
+  return new_array
+end
